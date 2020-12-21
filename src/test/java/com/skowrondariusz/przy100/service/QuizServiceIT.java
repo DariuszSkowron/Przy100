3a68736a161ac7932d87ec6667a59c087196cea5
Dariusz Skowron <dariusz.skowron@gmail.com>
2020-12-21 19:12:28 +0100

    QuizService integration testing

--- src/test/java/com/skowrondariusz/przy100/service/QuizServiceIT.java
+++ src/test/java/com/skowrondariusz/przy100/service/QuizServiceIT.java
@@ -1,4 +1,30 @@
 package com.skowrondariusz.przy100.service;
 
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@TestPropertySource("classpath:application-testing.properties")
 public class QuizServiceIT {
+
+
+
+    @Autowired
+    QuizService quizService;
+
+
+
+    @Test
+    public void shouldStartNewQuiz(){
+        assertThat(quizService.startNewQuiz(5).getStartTime()).isNotNull();
+    }
+
 }
