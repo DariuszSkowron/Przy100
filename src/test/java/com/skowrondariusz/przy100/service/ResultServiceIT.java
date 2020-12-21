6f0bf67976d6d2ac3cf2ab4f0a1b195f13f698b5
Dariusz Skowron <dariusz.skowron@gmail.com>
2020-12-21 20:01:37 +0100

    ResultService integration testing

--- src/test/java/com/skowrondariusz/przy100/service/ResultServiceIT.java
+++ src/test/java/com/skowrondariusz/przy100/service/ResultServiceIT.java
@@ -1,4 +1,42 @@
 package com.skowrondariusz.przy100.service;
 
+
+import com.skowrondariusz.przy100.model.Result;
+import com.skowrondariusz.przy100.repository.ResultRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@TestPropertySource("classpath:application-testing.properties")
 public class ResultServiceIT {
+
+
+    @Autowired
+    ResultService resultService;
+
+    @Autowired
+    ResultRepository resultRepository;
+
+
+
+    @Test
+    public void shouldReturnLowestSubmittedScore(){
+        Result result1 = new Result(20d,5,"test1", 555d);
+        resultRepository.save(result1);
+        Result result2 = new Result(31d,10,"test2", 111d);
+        resultRepository.save(result2);
+        assertThat(resultService.checkLastSubmittedScore()).isEqualTo(111d);
+    }
+
+    @Test
+    public void shouldReturnZeroWhenRepositoryIsEmptyAndLowestScoreWhenItsNot(){
+        assertThat(resultService.checkLastSubmittedScore()).isEqualTo(0d);
+    }
 }
